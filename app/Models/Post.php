19be67dae647ba36b352966afe8fcb68143338a9
Makhsudov <mr.makhsudov@gmail.com>
2023-02-28 18:14:00 +0500

    linking Models - Post, Comment, User

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -14,5 +16,16 @@ class Post extends Model
         'title',
         'short_content',
         'content',
-        'photo'];
+        'photo'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()  //: HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
